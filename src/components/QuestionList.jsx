754a8e86fd1522317c5bf05ef93596a9acfc511c
Albert Jablonsky <a.b.jablonsky@gmail.com>
2023-06-21 00:23:39 +0200

    Added svg logo, added new styles, add next button

--- src/components/QuestionList.jsx
+++ src/components/QuestionList.jsx
@@ -3,20 +3,31 @@ import pathQuestions from "../constants"
 
 const QuestionList = () => {
     const navigate = useNavigate();
-    
+
     const handleClick = (id) => {
         navigate(`/${id}`)
     }
 
     return (
-        <div className='w-[100vw] h-[100vh] bg-primary flex flex-col items-center justify-center p-3 gap-9'>
-            {pathQuestions.map((question) => (
-                <div className={`flex justify-center items-center bg-secondary rounded-md h-[50px] w-full text-white ${localStorage.getItem(question.id) ? 'correct-gradient' : ''}`} key={question.id} onClick={() => handleClick(question.id)}>
-                    {question.id}
+        <div className="flex justify-center items-center my-8 font-manrope text-white">
+            <div className=" w-[85%]">
+                <div className="max-w-[100%] max-h-[30%] flex justify-center items-center">
+                    <img className="w-full h-full" src={'one.png'}></img>
                 </div>
-            ))}
+                {/* <div className='text-xl text-center mt-5 mb-5'>Satalická rádiovka</div> */}
+                <div className="flex justify-center">
+                    <div className='w-[90%] bg-primary flex flex-col items-center justify-center px-3 mt-5 gap-5'>
+                        {pathQuestions.map((question) => (
+                            <div className={`flex justify-center items-center bg-secondary rounded-md h-10 w-full text-white ${localStorage.getItem(question.id) === 'true' ? 'correct-gradient' : ''}`} key={question.id} onClick={() => handleClick(question.id)}>
+                                {question.id}
+                            </div>
+                        ))}
+
+                    </div>
+                </div>
+            </div>
         </div>
-  )
+    )
 }
 
 export default QuestionList
--- src/components/QuestionPage.jsx
+++ src/components/QuestionPage.jsx
@@ -1,45 +1,56 @@
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom"
 
-const QuestionPage = ({question}) => {
+const QuestionPage = ({ question }) => {
     const navigate = useNavigate();
 
-    const handleClick = (option) => {
-        if(!option.correct){
-            alert("Špatná odpověď, zkus to znovu!")
+    const [clicked, setClicked] = useState('')
+    const [isAnswered, setIsAnswered] = useState(false)
+
+    useEffect(() => {
+        if(localStorage.getItem(question.id) === 'true'){
+            setIsAnswered(() => true)
         }
-        else{
-            if(question.id === 8){
-                alert("Congratulations, you won!")
-                localStorage.setItem(question.id, true)
-            }
-            else{
-                navigate(`/${question.id+1}`)
-                localStorage.setItem(question.id, true)
-            }
+    })
+    
+    const handleClick = (option) => {
+        
+        if(localStorage.getItem(question.id) !== 'true'){
+            setClicked(() => option.answer)
+        }
+
+        if (option.correct) {
+            localStorage.setItem(question.id, true)
         }
     }
 
-  return (
-    <div className="w-[100vw] h-[100vh] bg-primary text-white font-manrope p-3">
-    <Link to="/">Exit</Link>
-    <section className="w-full h-full bg-primary flex flex-col gap-10 items-center">
-        <div className="max-w-[100%] max-h-[30%] flex justify-center items-center mt-5">
-            <img className="w-full h-full" src={question.pic}></img>
-        </div>
-        <section className="flex flex-col justify-center gap-8">
-        <h1 className="text-center text-lg">{question.question}</h1>
-        <div className="flex flex-col gap-8 items-center justify-center">
-            {question.options.map((option) => (
-                <div className="w-[90%] h-16 bg-secondary rounded-md flex justify-center items-center" onClick={() => handleClick(option)}>
-                    {option.answer}
+    return (
+        <div className="flex justify-center p-5">
+            <div className="w-[90vw] bg-primary text-white font-manrope p-3">
+                <div className="px-1">
+                    <Link to="/">Home</Link>
+                    <div className=' float-right' hidden={question.id === 8}>
+                        <Link to={`/${question.id + 1}`}>Další</Link>
+                    </div>
                 </div>
-            ))}
+                <section className="w-full bg-primary flex flex-col gap-10 items-center">
+                    <div className="max-w-[100%] max-h-[30%] flex justify-center items-center mt-5">
+                        <img className="w-full h-full" src={'one.png'}></img>
+                    </div>
+                    <section className="flex flex-col justify-center gap-8 w-[100%]">
+                        <h1 className="text-center text-lg">{question.question}</h1>
+                        <div className="flex flex-col gap-8 items-center justify-center">
+                            {question.options.map((option) => (
+                                <div className={`w-[90%] h-16 bg-secondary rounded-md flex justify-center items-center ${isAnswered && option.correct ? 'correct-gradient' : ''} ${clicked === option.answer ? option.correct ? 'correct-gradient' : 'incorrect-gradient' : ''}`} onClick={() => handleClick(option)}>
+                                    {option.answer}
+                                </div>
+                            ))}
+                        </div>
+                    </section>
+                </section>
+            </div>
         </div>
-        </section>
-
-    </section>
-    </div>
-  )
+    )
 }
 
 export default QuestionPage
