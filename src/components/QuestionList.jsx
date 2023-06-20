578ba42192e2b0aeea952b1b815e74f97f51ea87
OlliJeCool <ollihomail@gmail.com>
2023-06-20 22:00:31 +0200

    added answer check functionality

--- src/components/QuestionList.jsx
+++ src/components/QuestionList.jsx
@@ -11,7 +11,7 @@ const QuestionList = () => {
     return (
         <div className='w-[100vw] h-[100vh] bg-primary flex flex-col items-center justify-center p-3 gap-9'>
             {pathQuestions.map((question) => (
-                <div className="flex justify-center items-center bg-secondary rounded-md h-[50px] w-full text-white" key={question.id} onClick={() => handleClick(question.id)}>
+                <div className={`flex justify-center items-center bg-secondary rounded-md h-[50px] w-full text-white ${localStorage.getItem(question.id) ? 'correct-gradient' : ''}`} key={question.id} onClick={() => handleClick(question.id)}>
                     {question.id}
                 </div>
             ))}
--- src/components/QuestionPage.jsx
+++ src/components/QuestionPage.jsx
@@ -1,6 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 const QuestionPage = ({question}) => {
+    const navigate = useNavigate();
+
+    const handleClick = (option) => {
+        if(!option.correct){
+            alert("Špatná odpověď, zkus to znovu!")
+        }
+        else{
+            if(question.id === 8){
+                alert("Congratulations, you won!")
+                localStorage.setItem(question.id, true)
+            }
+            else{
+                navigate(`/${question.id+1}`)
+                localStorage.setItem(question.id, true)
+            }
+        }
+    }
+
   return (
     <div className="w-[100vw] h-[100vh] bg-primary text-white font-manrope p-3">
     <Link to="/">Exit</Link>
@@ -12,7 +30,7 @@ const QuestionPage = ({question}) => {
         <h1 className="text-center text-lg">{question.question}</h1>
         <div className="flex flex-col gap-8 items-center justify-center">
             {question.options.map((option) => (
-                <div className="w-[90%] h-16 bg-secondary rounded-md flex justify-center items-center">
+                <div className="w-[90%] h-16 bg-secondary rounded-md flex justify-center items-center" onClick={() => handleClick(option)}>
                     {option.answer}
                 </div>
             ))}
